a0ea62a8e9b2ea1fac3efd8bfe6f34d39cb64c29
TheZoq2 <frans.skarman@gmail.com>
2017-07-11 18:23:37 +0200

    Update rspc demo script

--- demo.sh
+++ demo.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+OD=$(pwd)
+
 WD=/home/frans/Documents/rust/BspwmStackableWindows
+cd $WD
+cargo clean
+cd $OD
 
 clear
 
 #sleep 1
 
-echo "Tiling window managers are great"
+echo "Sometimes you have a program that you don't want to close"
 sleep 2
-echo "but they are not perfect if you have many rarley used terminals"
+echo "But you also don't want it to use too much space on your screen"
 sleep 2
-echo "I made a program that solves that issue in bspwm"
-sleep 3
+echo "Stacked windows are a nice way to get around that"
+sleep 2
 
 #Switch to another workspace to actually run the demo
 bspc desktop -f "focused:^5"
@@ -53,13 +58,13 @@ bspc node -f "east"
 bspc node -p south
 notify-send 'Stacks dont have to be horizontal'
 sleep 1
-xfce4-terminal --working-directory $WD &
+xfce4-terminal --working-directory $WD -x htop &
 sleep 0.3
 bspc node -f "@brother"
 bspc node -f "@1"
 bspc node -p north
 bspc node 
-xfce4-terminal --working-directory $WD &
+xfce4-terminal --working-directory $WD -x cargo build &
 sleep 0.3
 bspc node -f "@brother"
 bspc node -f "@1"
@@ -92,9 +97,10 @@ sleep 2
 notify-send 'You can also add new windows to the stack'
 sleep 1
 bspc node -p north
-xfce4-terminal --working-directory $WD &
+ksysguard &
+last_pid=$!
 sleep 1
-bspc node -f "south"
+bspc node -f "north"
 rspc stack focus_current
 sleep 1
 bspc node -f "south"
@@ -104,13 +110,23 @@ bspc node -f "north"
 rspc stack focus_current
 sleep 2
 
+notify-send 'Of course, individual windows can also be removed'
+sleep 1
+kill $last_pid
+rspc stack focus_current
+sleep 2
 
-# Go back to the default workspace
-bspc desktop -f "focused:^4"
+bspc node -f "south"
+rspc stack focus_current
+sleep 1
+bspc node -f "north"
+rspc stack focus_current
+sleep 1
+
 
 clear
-echo "The code and usage instructions can be found at github.com/thezoq2/BspwmStackableWindows"
-sleep 10
+# Go back to the default workspace
+bspc desktop -f "focused:^4"
 
 
 #xfce4-terminal --working-directory $WD
